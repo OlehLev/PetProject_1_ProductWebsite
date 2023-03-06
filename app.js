095688864d0af43c21a2767a296c0893707bc454
Oleh Levchenko <olehjurievich@gmail.com>
2023-03-06 21:47:27 +0200

    test mongoose

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-const as = []
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect( 'mongodb://localhost:27017',{ family: 4 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/users', console.log(true));
+
+
+app.listen(5000, () =>{
+    // eslint-disable-next-line no-console
+    console.log(`App listen 5000 `);
+});
