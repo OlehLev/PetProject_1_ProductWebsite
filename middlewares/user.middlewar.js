731f3763f638c46e8c46d229cc60df69cee267d7
Oleh Levchenko <olehjurievich@gmail.com>
2023-03-13 15:03:29 +0200

    added error handler and added step create user in middleware

--- middlewares/user.middlewar.js
+++ middlewares/user.middlewar.js
@@ -1,7 +1,23 @@
 const User = require('../dataBase/User');
+const { ErrorHandler } = require('../errors/ErrorHandler');
+const { EMAIL_ALREADY_EXISTS, WRONG_EMAIL_OR_PASSWORD } = require('../errors/errors.list');
 
 module.exports = {
-    createUserMiddleware: {},
+    createUserMiddleware: async (req, res, next) => {
+        try{
+            const { email } = req.body;
+            const checkUserEmail = await User.find({email});
+
+            if (checkUserEmail) {
+                throw new ErrorHandler(EMAIL_ALREADY_EXISTS.message, EMAIL_ALREADY_EXISTS.status);
+            };
+            
+            next();
+        }catch(e) {
+            next(e);
+        }
+    },
+    
     isUserPresent:  async (req, res, next) => {
         try{
 
@@ -9,6 +25,10 @@ module.exports = {
                 .findOne({ email: req.body.email })
                 .select('+password')
                 .lean();
+
+            if (!userUniqueEmail) {
+                throw new ErrorHandler(WRONG_EMAIL_OR_PASSWORD.message, WRONG_EMAIL_OR_PASSWORD.status);
+            }
 
             req.user = userUniqueEmail;
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 
 const userController = require('../controllers/user.controller');
 const authMiddlewar = require('../middlewares/auth.middlewares');
+const userMiddlewar = require('../middlewares/user.middlewar');
 
 router.get("/", userController.getUsers);
-router.post("/", userController.createUser);
+router.post("/", 
+    userMiddlewar.createUserMiddleware,
+    userController.createUser);
 router.delete("/", 
     authMiddlewar.chackAccessToken,
     authMiddlewar.deleteUserToken,
