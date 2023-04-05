edccdb062bf276db09e341523c3d5b03c3b0ad5e
Oleh Levchenko <olehjurievich@gmail.com>
2023-04-05 20:17:52 +0300

    added user price with a discount on route get products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,14 +1,43 @@
 const Product = require('../dataBase/Products');
+const User = require('../dataBase/User');
+const { ErrorHandler } = require('../errors/ErrorHandler');
+const { ENTITY_NOT_FOUND } = require('../errors/errors.list');
 
 module.exports = {
     getProducts: async (req, res, next) => {
         try{
-            const allProduct = await Product.find({});
+            const allProduct = await Product.find();
+            
             res.send(allProduct);
         }catch(e){
             next(e);
         }
     },
+
+    getUserProducts: async (req, res, next) => {
+        try{
+            const allProduct = await Product.find();
+            const userId = req.user._id;
+
+            const user = await User.findOne({_id: userId});
+
+            if(!user) {
+                throw new ErrorHandler(ENTITY_NOT_FOUND.message, ENTITY_NOT_FOUND.status);
+            };
+
+            const userDiscount = (100 - user.discount)/100;
+
+            allProduct.forEach((e, i) => {
+                e._doc.user_price = e.product_price * userDiscount;
+                allProduct[i] = e;              
+            });
+            
+            res.send(allProduct);
+        }catch(e){
+            next(new ErrorHandler(ENTITY_NOT_FOUND.message, ENTITY_NOT_FOUND.status));
+        }
+    },
+
     createProduct: async (req, res, next) => {
         try{
             await Product.create({...req.body});
